a95e76f22aaed338bde5657e266a282d034dc796
Laura Hughes <flaneuse@users.noreply.github.com>
2017-09-22 20:10:25 -0400

    end of prelim models

--- 03_DHS_stunting.R
+++ 03_DHS_stunting.R
@@ -8,6 +8,7 @@ library(haven)
 library(tidyverse)
 library(svywrangler)
 library(modelr)
+library(llamar)
 
 data_dir = '~/Documents/USAID/Niger/NER_2012_DHS/'
 
@@ -151,7 +152,7 @@ kids = kids_raw %>%
 
 
 # Merge in some missing hh-level vars -------------------------------------
-# source('03_DHS_wealthidx.R')
+source('03_DHS_wealthidx.R')
 # TLUs and water shortage w/i past 2 weeks
 
 kids = hh %>% select(cluster, hh_num, TLU, water_shortage) %>% 
@@ -180,7 +181,7 @@ kids = kids %>%
   # Converting "other" and "not dejure resident" to NA
   replace_missing(missing_codes = c(96, 97, 99), drinking_src, toilet_src) %>% 
   replace_missing(missing_codes = c(997, 998), time2drinking) %>% 
-  replace_missing(missing_codes = c(98), agehead) %>% 
+  replace_missing(missing_codes = c(98, 99), agehead, num_otherwives) %>% 
   replace_missing(missing_codes = 9996:9999, mom_stunting, mom_bmi, mom_rohrer,
                   stunting, wasting, underweight, bmi) %>% 
   replace_missing(missing_codes = c(8, 9), num_otherwives, birth_size, diarrhea, cough, fever,
@@ -204,14 +205,17 @@ kids = kids %>%
          dhs_WI_rural = dhs_WI_rural/1e5,
          stunting = stunting/1e2,
          wasting = wasting/1e2,
-         underweight = underweight/1e2
+         underweight = underweight/1e2,
+         mom_rohrer = mom_rohrer/1e2,
+         mom_bmi = mom_bmi/1e2
          )
 
 
 # factorize and lump factors ----------------------------------------------
 # Also bin numeric values
 kids = kids %>% 
-  factorize('_lab', toilet_src, drinking_src) %>% 
+  factorize('_lab', toilet_src, drinking_src, region, sex, femhead) %>% 
+  
   # lump factors
   mutate(impr_toilet = ifelse(is.na(toilet_src), NA,
                                     ifelse(toilet_src %in% impr_toilet_codes, 1, 0)),
@@ -222,7 +226,12 @@ kids = kids %>%
          # fix shared toilet; assuming if you poop in the bushes that it's shared.
          shared_toilet = ifelse(toilet_src_lab == "No facility/bush/field", 1, shared_toilet),
          # discarding "wash hands after eating"; not common question
-         wash_knowl = washknow_foodprep + washknow_food + washknow_feed + washknow_toilets + washknow_diaper
+         wash_knowl = washknow_foodprep + washknow_food + washknow_feed + washknow_toilets + washknow_diaper,
+         
+         # calc squared age
+         age_months_sq = age_months^2,
+         # convert to factor
+         interview_month = factor(interview_month, levels = c(5,2:4,6, 7))
          )
 
 
@@ -238,13 +247,13 @@ all_stunting = kids  %>%
   center_scale()
 
 zinder = kids %>% 
-  filter(region == 'Zinder', 
+  filter(region_lab == 'Zinder', 
          !is.na(stunting),
          rural == 1) %>% 
   center_scale()
 
 # create basic models -----------------------------------------------------
-
+# Note: DHS wealth index includes WASH vars.
 models = formulas(~stunting,
                   wash = 
                     ~ # WASH
@@ -254,18 +263,49 @@ models = formulas(~stunting,
                   ,
                   basic = 
                     # basic demographics
-                    ~ sex + age_months*age_months +
+                    ~ sex_lab + age_months_sq +
+                    birth_order +
+                    interview_month +
                     # hh demographics
                     region_lab + hhsize + kids_under5 +
+                    femhead_lab +
                     
-                    # Wealth
-                    dhs_WI_rural
+                    # mother
+                    mom_rohrer + # corpulence idx, similar to BMI but maybe better
+                    num_otherwives +
+                    
+                    # Wealth 
+                    dhs_WI_rural + TLU
                   ,
                   combo = add_predictors(basic, wash))
 
+zinder-models = formulas(~stunting,
+                  wash = 
+                    ~ # WASH
+                    impr_toilet + shared_toilet + 
+                    impr_water + wash_knowl + diarrhea +
+                    water_shortage # within last 2 weeks
+                  ,
+                  basic = 
+                    # basic demographics
+                    ~ sex_lab + age_months_sq +
+                    birth_order +
+                    interview_month +
+                    # hh demographics
+                    region_lab + hhsize + kids_under5 +
+                    femhead_lab +
+                    
+                    # mother
+                    mom_rohrer + # corpulence idx, similar to BMI but maybe better
+                    num_otherwives +
+                    
+                    # Wealth 
+                    dhs_WI_rural + TLU
+                  ,
+                  combo = add_predictors(basic, wash))
 # Stunting z-score
 all_z = all_stunting %>% fit_with(lm, models)
-zinder_z = zinder %>% fit_with(lm, models)
+zinder_z = zinder %>% fit_with(lm, zinder_models)
 
 # Binary stunted
 # all_stunted = all_stunting %>% fit_with(glm, models_stunted, family = binomial)
@@ -274,6 +314,13 @@ zinder_z = zinder %>% fit_with(lm, models)
 
 # summary -----------------------------------------------------------------
 summary(all_z$combo)
+summary(zinder_z$combo)
 
 plot_coef(all_z$combo)
+plot_coef(zinder_z$combo)
+
+
+
+# export data for Tim. ----------------------------------------------------
+write.csv(all_z, '~/Documents/USAID/Niger/data/NER_DHS_semicleankidsdata_2017-09-22.csv')
 
